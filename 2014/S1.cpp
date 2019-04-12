aa28540161a72a59ae49fca3511056c3db27b115
Ryan <ryan.lichangrui@gmail.com>
2019-04-11 21:18:26 -0400

    git commit -m "partially completed 2014 S1, added Cmakelists to gitignore"

--- 2014/S1.cpp
+++ 2014/S1.cpp
@@ -1,4 +1,28 @@
-//
-// Created by ryanl on 4/11/2019.
-//
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main(){
+    int k, m, a;
+    vector<int>friends{};
+
+    cin >> k >> m;
+
+    for (int i = 1; i < k+1; i++){
+        friends.push_back(i);
+    }
+
+    for (int i = 0; i < m; i++){
+        cin >> a;
+        for (int j = 0; j < friends.size(); j++) {
+            if ((j+1) % a == 0) {
+                friends.erase(friends.begin()+j);
+            }
+        }
+    }
+
+    for (int i = 0; i < friends.size(); i++){
+        cout << friends[i] << endl;
+    }
+}
