7aee100b04c11897941460898f663a4fee2636cc
Ryan <ryan.lichangrui@gmail.com>
2019-04-19 14:22:43 -0400

    updated 2018 J4, still not completed

--- 2018/J4.cpp
+++ 2018/J4.cpp
@@ -2,113 +2,54 @@
 
 using namespace std;
 
-
 int main(){
-    int n, a,rt;
+    int n, a;
 
     cin >> n;
-    int flowers[n][n];
+    int flowers[110][110];
 
     //takes grid input
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j ++){
-            cin >> a;
-            flowers[i][j] = a;
-        }
-    }
-
-    //loops 4 times
-    for (int k = 0; k < 4; k++){
-
-        //checks the grid left to right
-        for (int i = 0; i < n; i++){
-
-            bool order = true;
-            int lowest = 0;
-
-            for (int j = 0; j < n; j++) {
-                //checks to see if the grid is in order
-                if (flowers[i][j] < lowest) {
-                    order = false;
-                    break;
-                }
-                lowest = flowers[i][j];
-            }
-            //if not in order, break and move on to the next loop
-            if (!order){
-                break;
-            }
-            //if in order, set the rotation to 0 and break
-            else {
-                rt = 0;
-                break;
-            }
-        }
-        //checks the grid top to bottom
-        for (int i = 0; i < n; i++){
-            bool order = true;
-            int lowest = 0;
             for (int j = 0; j < n; j++){
-                if (flowers[j][i] < lowest){
-                    order = false;
-                    break;
-                }
-                lowest = flowers[j][i];
+                cin >> a;
+                flowers[i][j] = a;
             }
-            if (!order){
-                break;
-            }
-            else {
-                rt = 1;
-                break;
-            }
-        }
-
-        //checks the grid right to left
-        for (int i = n; i > 0; i--){
-            bool order = true;
-            int lowest = 0;
-            for (int j = n; j > 0; j--){
-                if (flowers[i][j] < lowest){
-                    order = false;
-                    break;
-                }
-                lowest = flowers[i][j];
-            }
-            if (!order){
-                break;
-            }
-            else {
-                rt = 2;
-            }
-        }
-        //checks the grid bottom to top
-        for (int i = n; i > 0; i--){
-            bool order = true;
-            int lowest = 0;
-            for (int j = n; j > 0; j--){
-                if (flowers[j][i] < lowest) {
-                    order = false;
-                    break;
-                }
-                lowest = flowers[j][i];
-            }
-            if (!order){
-                break;
-            }
-            else {
-                rt = 3;
-                break;
-            }
-        }
     }
 
-    cout << rt << endl;
-    //prints out the grid
-//    for (int i = 0; i < n; i++){
-//        for (int j = 0; j < n; j++){
-//            cout << flowers[i][j] << " ";
-//        }
-//        cout << "\n";
-//    }
+    //grid is not rotated
+    if (flowers[0][0] < flowers[0][1] && flowers[0][0] < flowers[1][0]){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                cout << flowers[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    //grid is rotated 90 degrees
+    if (flowers[0][n-1] < flowers[1][n-1] && flowers[0][n-1] < flowers[0][n-2]){
+        for (int i = n-1; i >= 0; i--){
+            for (int j = 0; j < n; j++){
+                cout << flowers[j][i] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    //grid is rotated 180 degrees
+    if (flowers[n-1][n-1] < flowers[n-2][n-1] && flowers[n-1][n-1] < flowers[n-1][n-2]){
+        for (int i = n-1; n >= 0; i--){
+            for (int j = n-1; j >= 0; j--){
+                cout << flowers[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    //grid is rotated 270 degrees
+    if (flowers[n-1][0] < flowers[n-2][0] && flowers[n-1][0] < flowers[n-1][1]){
+        for (int i = 0; i < n; i++){
+            for (int j = n-1; j >= 0; j--){
+                cout << flowers[j][i] << " ";
+            }
+            cout << "\n";
+        }
+    }
 }
