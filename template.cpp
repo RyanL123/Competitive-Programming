5b984232aeb77095374aec61742c668986984ae2
Ryan Li <ryan.lichangrui@gmail.com>
2020-02-13 11:12:43 -0500

    added cpp template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <utility>
+#include <numeric>
+#include <queue>
+using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb(a, b) push_back(a, b)
+#define eb(a, b) emplace_back(a, b)
+#define inf INT_MAX
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+}
