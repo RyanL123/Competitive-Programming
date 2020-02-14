3f3a1475e8fb55fe39ec84137d5727dabf7597a4
Ryan <ryan.lichangrui@gmail.com>
2020-02-14 18:40:49 -0500

    completed Mock CCC 2020 Contest 1 Junior J3

--- DMOJ Contests/Mock CCC 2020 Contest 1 Junior/J3.cpp
+++ DMOJ Contests/Mock CCC 2020 Contest 1 Junior/J3.cpp
@@ -3,27 +3,12 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
-#include <cmath>
 
 using namespace std;
 
 int main() {
-    unsigned long long x, y;
+    long double x, y;
     cin >> x >> y;
-    unsigned long long area = x*y;
-    if (x % 4 == 0 || y % 4 == 0){
-        cout << (x*y)/4 << ".00" << endl;
-    }
-    else if (x % 2 == 0 && y % 2 == 0){
-        cout << (x*y)/4 << ".00" << endl;
-    }
-    else if (x % 2 == 0 || y % 2 == 0){
-        cout << (x*y)/4 << ".50" << endl;
-    }
-    else if ((double)x / 4 - floor(x/4) == 0.25 && (double)y / 4 - floor(y/4) == 0.25){
-        cout << (x*y)/4 << ".25" << endl;
-    }
-    else {
-        cout << (x*y)/4 << ".75" << endl;
-    }
+    long double ans = x*y/4;
+    printf("%.2Lf\n", ans);
 }
