b46257357b7f7b869ddbcfddb2b5cad1bb5f89eb
Ryan <ryan.lichangrui@gmail.com>
2020-02-13 21:36:26 -0500

    completed knapsack 2

--- Dynamic Programming/Knapsack/Knapsack 2.cpp
+++ Dynamic Programming/Knapsack/Knapsack 2.cpp
@@ -1,67 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb(a, b) push_back(a, b)
+#define eb(a, b) emplace_back(a, b)
+#define inf INT_MAX
 
-
+ll dp[110][100010];
 int main() {
-    string dp[2] = {"", ""};
-    long n, w;
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    int n, w;
     cin >> n >> w;
-
-    // Get objects
-    long objWeight[101];
-    long objVal[101];
+    int weight[n];
+    int val[n];
     for (int i = 0; i < n; i++){
-        cin >> objWeight[i];
-        cin >> objVal[i];
+        cin >> weight[i] >> val[i];
     }
-    string s(1000000000, ' ');
-    dp[0] = s;
-    dp[1] = s;
-
-    //DP
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= w; j++){
-            if (i%2 == 0){
-                // Do not take the object
-                long notTaken = dp[1][j];
-                long taken;
-                // Item can be considered
-                if (objWeight[i-1] <= j){
-                    taken = dp[1][j-objWeight[i-1]] + objVal[i-1];
-                }
-                else {
-                    // Cannot take item
-                    dp[0][j] = dp[1][j];
-                    continue;
-                }
-                // Take the most optimal choice
-                dp[0][j] = (char)max(taken, notTaken);
-            }
-            else {
-                // Do not take the object
-                long notTaken = dp[0][j];
-                long taken;
-                // Item can be considered
-                if (objWeight[i-1] <= j){
-                    taken = dp[0][j-objWeight[i-1]] + objVal[i-1];
-                }
-                else {
-                    // Cannot take item
-                    dp[1][j] = dp[0][j];
-                    continue;
-                }
-                // Take the most optimal choice
-                dp[1][j] = (char)max(taken, notTaken);
-            }
+    for (int i = 0; i <= n; i++){
+        for (int j = 1; j <= 100000; j++){
+            dp[i][j] = 10000000000;
         }
     }
-    if (n%2 == 0){
-        cout << (int)dp[0][w];
+    // Items taken
+    for (int i = 1; i <= n; i++){
+        // Value of j
+        for (int j = 1; j <= 100000; j++){
+            ll notTake = dp[i-1][j];
+            ll take;
+            if (val[i-1] <= j){
+                take = weight[i-1] + dp[i-1][j-val[i-1]];
+            }
+            else {
+                dp[i][j] = notTake;
+                continue;
+            }
+            dp[i][j] = min(notTake, take);
+        }
     }
-    else {
-        cout << (int)dp[1][w];
+    int maxVal = 0;
+    for (int i = 0; i <= 100000; i++){
+        if (dp[n][i] <= w && i > maxVal){
+            maxVal = i;
+        }
     }
+    cout << maxVal << endl;
 }
