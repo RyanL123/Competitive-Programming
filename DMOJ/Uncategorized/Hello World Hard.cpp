6724eabcc0b453477f7ac6b27a9fef007e069a8f
Ryan Li <ryan.lichangrui@gmail.com>
2020-02-23 19:10:36 -0500

    completed hello world hard

---
+++ DMOJ/Uncategorized/Hello World Hard.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define inf INT_MAX
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    // This works but I don't know WHY it works
+    cout << "Hello, World!\n";
+    cout << "Hello, World!\r";
+    cout << "Hello, World!\r\n";
+}
