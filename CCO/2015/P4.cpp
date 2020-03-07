e4066dfb2a2c0efeef6dab7ce83cb423d6f74d02
Ryan Li <ryan.lichangrui@gmail.com>
2020-03-07 14:29:31 -0500

    completed CCO2015P4

--- CCO/2015/P4.cpp
+++ CCO/2015/P4.cpp
@@ -10,101 +10,58 @@ typedef unsigned long long ull;
 #define inf INT_MAX
 
 string parking[2010];
-vii graph[2010][2010];
-int outDeg[2010][2010];
+int n, m;
+
+void solve(char dir, int r, int c){
+    if (dir == 'N'){
+        for (int i = r-1; i >= 0; i--){
+            if (parking[i][c] != '.'){
+                solve(parking[i][c], i, c);
+            }
+        }
+        parking[r][c] = '.';
+        cout << "(" << r << "," << c << ")\n";
+    }
+    else if (dir == 'S'){
+        for (int i = r+1; i < n; i++){
+            if (parking[i][c] != '.'){
+                solve(parking[i][c], i, c);
+            }
+        }
+        parking[r][c] = '.';
+        cout << "(" << r << "," << c << ")\n";
+    }
+    else if (dir == 'E'){
+        for (int i = c+1; i < n; i++){
+            if (parking[r][i] != '.'){
+                solve(parking[r][i], r, i);
+            }
+        }
+        parking[r][c] = '.';
+        cout << "(" << r << "," << c << ")\n";
+    }
+    else if (dir == 'W') {
+        for (int i = c-1; i >= 0; i--){
+            if (parking[r][i] != '.'){
+                solve(parking[r][i], r, i);
+            }
+        }
+        parking[r][c] = '.';
+        cout << "(" << r << "," << c << ")\n";
+    }
+}
+
 int main() {
     cin.sync_with_stdio(0);
     cin.tie(0);
-    int n, m;
     cin >> n >> m;
-    int cars = 0;
     for (int i = 0; i < n; i++){
         cin >> parking[i];
     }
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
-            if (parking[i][j] == 'N'){
-                int it = i-1;
-                bool firstCar = true;
-                while (it >= 0){
-                    if (parking[it][j] != '.'){
-                        if (firstCar){
-                            graph[it][j].pb(mp(i, j));
-                            firstCar = false;
-                        }
-                        outDeg[i][j]++;
-                    }
-                    it--;
-                }
-                cars++;
-            }
-            else if (parking[i][j] == 'S'){
-                int it = i+1;
-                bool firstCar = true;
-                while (it < n){
-                    if (parking[it][j] != '.'){
-                        if (firstCar){
-                            graph[it][j].pb(mp(i, j));
-                            firstCar = false;
-                        }
-                        outDeg[i][j]++;
-                    }
-                    it++;
-                }
-                cars++;
-            }
-            else if (parking[i][j] == 'W'){
-                int it = j-1;
-                bool firstCar = true;
-                while (it >= 0){
-                    if (parking[i][it] != '.'){
-                        if (firstCar){
-                            graph[i][it].pb(mp(i, j));
-                            firstCar = false;
-                        }
-                        outDeg[i][j]++;
-                    }
-                    it--;
-                }
-                cars++;
-            }
-            else if (parking[i][j] == 'E'){
-                int it = j+1;
-                bool firstCar = true;
-                while (it < m){
-                    if (parking[i][it] != '.'){
-                        if (firstCar){
-                            graph[i][it].pb(mp(i, j));
-                            firstCar = false;
-                        }
-                        outDeg[i][j]++;
-                    }
-                    it++;
-                }
-                cars++;
-            }
-        }
-    }
-    queue<pii> q;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            if (parking[i][j] != '.' && outDeg[i][j] == 0){
-                q.push(mp(i,j));
-            }
-        }
-    }
-    int count = 0;
-    while (!q.empty()){
-        pii node = q.front();
-        q.pop();
-        outDeg[node.first][node.second]--;
-        cout << "(" << node.first << "," << node.second << ")\n";
-        count++;
-        for (int l = 0; l < graph[node.first][node.second].size(); l++){
-            pii children = graph[node.first][node.second][l];
-            outDeg[children.first][children.second]--;
-            if (outDeg[children.first][children.second] == 0){
-                q.push(mp(children.first, children.second));
+            if (parking[i][j] != '.'){
+                solve(parking[i][j], i, j);
             }
         }
     }
