e788c82cd2f0fc65e5d790345b25ad39b734773a
Ryan <ryan.lichangrui@gmail.com>
2020-03-14 15:56:08 -0400

    added code forces r628 div 2 files

---
+++ Code Forces/Round 628 Div 2/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
+#define inf 0x3f3f3f3f
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--){
+        int x;
+        cin >> x;
+        cout << 1 << " " << x-1 << endl;
+    }
+}
