4831a17fcc1a1c8c1172c8c30b28769911d7bdd4
Ryan Li <ryan.lichangrui@gmail.com>
2020-03-25 19:03:01 -0400

    Change 2020 S1 to use printf

--- CCC/2020/S1.cpp
+++ CCC/2020/S1.cpp
@@ -8,25 +8,22 @@
 
 using namespace std;
 
-double dis[200010];
 int main() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);
     int n;
     cin >> n;
-    double speed = 0;
-    vector<pair<int, int>> dis;
+    long double speed = 0;
+    vector<pair<long double, long double>> dis;
     for (int i = 0; i < n; i++){
-        double a, b;
+        long double a, b;
         cin >> a >> b;
         dis.push_back(make_pair(a, b));
     }
     sort(dis.begin(), dis.end());
-    for (int i = 1; i < dis.size(); i++){
-        double speed1 = (double)abs(dis[i].second - dis[i-1].second)/abs(dis[i].first-dis[i-1].first);
+    for (int i = 1; i < n; i++){
+        long double speed1 = abs((dis[i].second - dis[i-1].second)/(dis[i].first-dis[i-1].first));
         if (speed1 > speed){
             speed = speed1;
         }
     }
-    cout << speed << endl;
+    printf("%Lf", speed);
 }
