d57b2203c49563c49818a90b8f16024f14973dae
Ryan <ryan.lichangrui@gmail.com>
2020-04-05 12:54:10 -0400

    complete dmopc19c6p2 and dmopc19c6p3

--- DMOJ C++/Live Contests/DMOPC 19 March/dmopc19c6p2.cpp
+++ DMOJ C++/Live Contests/DMOPC 19 March/dmopc19c6p2.cpp
@@ -10,57 +10,37 @@ typedef unsigned long long ull;
 #define mp(a, b) make_pair(a, b)
 #define inf 0x3f3f3f3f
 
-bool sieve[1000010];
-int factCnt[1000010];
+int cnt[1000010];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int a, b;
     cin >> a >> b;
-    for (int i = 0; i <= 1000000; i++) sieve[i] = true;
-    for (int i = 2; i*i <= 1000000; i++){
-        if (sieve[i]){
-            for (int j = i*i; j <= 1000000; j += i){
-                sieve[j] = false;
-            }
+    vi A;
+    for (int i = 2; i <= ceil(sqrt(a)); i++){
+        if (a % i == 0){
+            A.pb(i);
+        }
+        while (a % i == 0){
+            a /= i;
+            cnt[i]++;
         }
     }
-    int num = a;
-    while (num % 2 == 0){
-        num /= 2;
-        factCnt[2]++;
+    if (a != 1){
+        A.pb(a);
+        cnt[a]++;
     }
-    for (int i = 3; i <= ceil(sqrt(num))+1; i++){
-        while (num % i == 0){
-            factCnt[i]++;
-            num /= i;
+    int n = inf;
+    for (int i : A){
+        int factCnt = 0;
+        int target = i;
+        while (b / target != 0){
+            factCnt += b/target;
+            target *= i;
         }
+        int occurrences = factCnt / cnt[i];
+        n = min(n, occurrences);
     }
-    if (num > 2) factCnt[num]++;
-    vi factor;
-    for (int i = 1; i <= ceil(sqrt(a))+1; i++){
-        if (a % i == 0 && sieve[i]){
-            if (find(factor.begin(), factor.end(), i) == factor.end()){
-                factor.pb(i);
-            }
-            if (sieve[a/i] && find(factor.begin(), factor.end(), a/i) == factor.end()){
-                factor.pb(a/i);
-            }
-        }
-    }
-    ull d = LONG_LONG_MAX;
-    for (int i = 0; i < factor.size(); i++){
-        ull tot = 0;
-        int fact = b;
-        if (factor[i] == 1) continue;
-        while (true){
-            fact = floor(fact/factor[i]);
-            tot += fact;
-            if (fact < 1) break;
-        }
-        tot = floor(tot/factCnt[factor[i]]);
-        d = min(tot, d);
-    }
-    cout << d << "\n";
+    cout << n;
 }
--- DMOJ C++/Live Contests/DMOPC 19 March/dmopc19c6p3.cpp
+++ DMOJ C++/Live Contests/DMOPC 19 March/dmopc19c6p3.cpp
@@ -10,6 +10,8 @@ typedef unsigned long long ull;
 #define mp(a, b) make_pair(a, b)
 #define inf 0x3f3f3f3f
 
+ll pw[500010];
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -17,19 +19,29 @@ int main() {
     ll MOD = 1e9+7;
     string n;
     cin >> s >> m >> n;
-    ull ans = 0;
+    set<int> ind;
+    pw[0] = 1;
+    for (int i = 1; i <= 500000; i++){
+        pw[i] = (pw[i-1] + pw[i-1])%MOD;
+    }
+    ll ans = 0;
     for (int i = 0; i < s; i++){
-        if (n[i] == '1') ans += (ll)pow(2, s-i-1)%MOD;
+        if (n[i] == '1'){
+            ans = (ans + pw[s-i-1])%MOD;
+        }
+        else {
+            ind.insert(i+1);
+        }
     }
     for (int i = 0; i < m; i++){
         int l, r;
         cin >> l >> r;
-        for (int j = l-1; j < r; j++){
-            if (n[j] != '1'){
-                ans += (ll)pow(2, s-j-1)%MOD;
-                n[j] = '1';
-            }
+        int x = *ind.lower_bound(l);
+        while (x <= r && !ind.empty() && ind.lower_bound(l) != ind.end()){
+            ans = (ans + pw[s-x])%MOD;
+            ind.erase(x);
+            x = *ind.lower_bound(l);
         }
-        cout << ans%MOD << "\n";
+        cout << ans << "\n";
     }
 }
