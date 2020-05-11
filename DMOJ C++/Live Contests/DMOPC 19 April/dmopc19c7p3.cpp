1d601de4469d18f2900ecfc1b01630a25adc1b25
Ryan Li <ryan.lichangrui@gmail.com>
2020-05-11 15:22:40 -0400

    complete dmopc19c7p3

--- DMOJ C++/Live Contests/DMOPC 19 April/dmopc19c7p3.cpp
+++ DMOJ C++/Live Contests/DMOPC 19 April/dmopc19c7p3.cpp
@@ -12,53 +12,62 @@ typedef unsigned long long ull;
 #define srt(x) sort(x.begin(), x.end())
 #define inf 0x3f3f3f3f
 const int MM = 1e6+5;
-ll w[MM], szw[MM];
-int sz[MM], inv[MM];
-vi adj[MM];
 
-void dfs(int v, int p){
-    szw[v] = w[v];
-    sz[v] = 1;
-    for (int u : adj[v]){
-        if (u != p){
-            dfs(u, v);
-            szw[v] += szw[u];
-            sz[v] += sz[u];
+ll w[MM], n, k;
+bool valid[MM], visited[MM];
+vi adj[MM], ans;
+
+
+bool bfs(int i){
+    visited[i] = true;
+    queue<int> q;
+    q.push(i);
+    ans.pb(i);
+    ll tot = w[i];
+    while (!q.empty()){
+        int v = q.front();
+        q.pop();
+        for (int u : adj[v]){
+            if (w[u] + tot <= 2*k && !visited[u]){
+                ans.pb(u);
+                tot += w[u];
+                visited[u] = true;
+            }
         }
     }
+    return tot >= k && tot <= 2 * k;
 }
 
-void dfs2(int v, int p){
-    inv[v] = 1;
-    for (int u : adj[v]){
-        if (u != p && sz[u] < sz[v]) dfs2(u, v);
-    }
-}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, k;
-    ll tot = 0;
     cin >> n >> k;
     for (int i = 1; i <= n; i++){
         cin >> w[i];
-        tot += w[i];
+        if (w[i] >= k && w[i] <= 2*k){
+            cout << "1\n";
+            cout << i << '\n';
+            return 0;
+        }
+        if (w[i] <= 2*k) valid[i] = true;
     }
     for (int i = 1; i < n; i++){
         int x, y;
         cin >> x >> y;
-        adj[x].pb(y);
-        adj[y].pb(x);
-    }
-    dfs(1, 0);
-    for (int i = 1; i <= n; i++){
-        if (tot - szw[i] >= k && tot - szw[i] <= k*2){
-            cout << n-sz[i] << '\n';
-            dfs2(i, 0);
-            for (int j = 1; j <= n; j++){
-                if (inv[j] == 0) cout << j << ' ';
-            }
-            return 0;
+        if (valid[x] && valid[y]){
+            adj[x].pb(y);
+            adj[y].pb(x);
         }
     }
+    for (int i = 1; i <= n; i++){
+        if (valid[i] && !visited[i]){
+            if (bfs(i)){
+                cout << ans.size() << '\n';
+                for (int j : ans) cout << j << ' ';
+                return 0;
+            }
+            else ans.clear();
+        }
+    }
+    cout << "-1\n";
 }
