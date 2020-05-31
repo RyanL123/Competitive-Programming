f4b6c012760bfd2b3322171e745544bdef5a6620
Ryan Li <ryan.lichangrui@gmail.com>
2020-05-31 12:03:11 -0400

    update 2019 S2

--- CCC C++/2019/S2.cpp
+++ CCC C++/2019/S2.cpp
@@ -1,36 +1,46 @@
-#include <iostream>
-#include <cstdio>
-#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define srt(x) sort(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define inf 0x3f3f3f3f
 
-//checks if integer is prime
-bool isPrime(int a){
-  for (int b = 2; b < ceil(sqrt(a))+1; b++){
-    if (a%b == 0){
-      return false;
+const int MM = 2e6+5;
+bool prime[MM];
+
+void sieve(){
+    for (int i = 2; i*i < MM; i++){
+        if (prime[i]){
+            for (int j = i*i; j < MM; j += i){
+                prime[j] = false;
+            }
+        }
     }
-  }
-  return true;
 }
 
 int main() {
-  //takes input
-  int t,input;
-  scanf("%i", &t);
-
-  //finds average of primes
-  for (int k = 0; k < t; k++){
-    scanf("%i", &input);
-
-    int a, b;
-    for (int i = 2; i < 2*input; i++){
-      a = i;
-      b = 2*input-i;
-      if (isPrime(a) && isPrime(b) && (a+b) == 2*input){
-        printf("%i %i\n", a , b);
-        break;
-      }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    memset(prime, true, sizeof(prime));
+    sieve();
+    while (t--){
+        int n;
+        cin >> n;
+        for (int i = 2; i < 2*n; i++){
+            if (prime[i] && prime[2*n-i]){
+                cout << i << ' ' << 2*n-i << '\n';
+                break;
+            }
+        }
     }
-  }
-} 
+}
