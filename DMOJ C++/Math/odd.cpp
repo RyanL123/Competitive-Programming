cbb4f46da26c77fb9b8e333ec9081ca1426c5bf5
Ryan Li <ryan.lichangrui@gmail.com>
2020-05-31 12:03:25 -0400

    complete odd

---
+++ DMOJ C++/Math/odd.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define srt(x) sort(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define inf 0x3f3f3f3f
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    int ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        ans ^= x;
+    }
+    cout << ans << "\n";
+}
