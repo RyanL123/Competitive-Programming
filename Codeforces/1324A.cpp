03f7b151d4ed02a0b23ceb51f27ca43fda179427
Ryan <ryan.lichangrui@gmail.com>
2020-06-25 18:33:40 -0400

    reorg codeforces files

--- Codeforces/1324A.cpp
+++ Codeforces/1324A.cpp
@@ -1,36 +1,43 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 typedef pair<int, int> pii;
 typedef vector<pair<int, int>> vii;
 typedef vector<int> vi;
-typedef long double ld;
 typedef long long ll;
 typedef unsigned long long ull;
-#define eb emplace_back
-#define pb push_back
-#define mp make_pair
-#define srt(x) sort(x.begin(), x.end())
-#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define mp(a, b) make_pair(a, b)
 #define inf 0x3f3f3f3f
 
-const int MM = 2e5+5;
-int a[MM], b[MM], n;
-ll ans;
-
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> n;
-    vi c;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) cin >> b[i];
-    for (int i = 0; i < n; i++) {
-        c.pb(a[i]-b[i]);
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        vi col;
+        int n;
+        cin >> n;
+        int highest = 0;
+        for (int j = 0; j < n; j++){
+            int a;
+            cin >> a;
+            if (a > highest) highest = a;
+            col.pb(a);
+        }
+        bool even = (highest-col[0])%2==0;
+        bool flag = false;
+        for (int j = 1; j < n; j++){
+            if (even && (highest-col[j])%2!=0){
+                flag = true;
+            }
+            else if (!even && (highest-col[j])%2==0){
+                flag = true;
+            }
+        }
+        if (flag) cout << "NO\n";
+        else cout << "YES\n";
     }
-    srt(c);
-    for (int i = 0; i < n; i++){
-        ans += c.end()-lower_bound(c.begin()+i+1, c.end(), -c[i]+1);
-    }
-    cout << ans << '\n';
 }
