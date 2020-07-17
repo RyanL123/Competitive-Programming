9f5633fe2659bb4f1430d289bcc09c20875a7d56
Ryan <ryan.lichangrui@gmail.com>
2020-07-17 11:28:27 -0400

    complete 1374D

--- Codeforces/1374D.cpp
+++ Codeforces/1374D.cpp
@@ -13,43 +13,29 @@ typedef unsigned long long ull;
 #define all(x) x.begin(), x.end()
 #define inf 0x3f3f3f3f
 
-// Find required amount to make divisible by K
-// Sort required amount from least to greatest
-// If there are two of the same required, add K to prevent dupes
-// Currently TLEs on case 5
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
     cin >> t;
     while (t--){
-        int n, k, ans = 0, x = 0;
+        int n, k;
+        ll mx = 0;
         cin >> n >> k;
-        int a[n];
-        priority_queue<int> pq;
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-            pq.push((a[i]%k==0?0:k-(a[i]%k)));
+        vi v;
+        map<ll, ll> m;
+        for (ll i = 0, x; i < n; i++) {
+            cin >> x;
+            if (x%k==0) continue;
+            m[k-x%k]++;
+            mx = max(mx, m[k-x%k]);
         }
-        int prev = -1;
-        while (!pq.empty()){
-            int b = pq.top();
-            pq.pop();
-            if (b == 0) continue;
-            else if (b == prev){
-                pq.push(b+k);
-            }
-            else {
-                int req = abs(k-b);
-                cout << "req: " << req << ' ';
-                ans += req-x+1;
-                x = req+1;
-                cout << "x: " << x << ' ';
-                prev = b;
+        ll ans = 0;
+        for (pair<ll, ll> p : m){
+            if (p.second == mx) {
+                ans = k*(p.second-1) + p.first + 1;
             }
         }
-        cout << '\n';
-        cout << ans << "\n";
+        cout << ans << '\n';
     }
 }
