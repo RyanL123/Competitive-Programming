fa78909bb3d4c1c62076ee8e1544425a51a0f9a4
Ryan <ryan.lichangrui@gmail.com>
2020-07-26 11:44:30 -0400

    redo 2015 s4

--- CCC C++/2015/S4.cpp
+++ CCC C++/2015/S4.cpp
@@ -1,83 +1,52 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <tuple>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define srt(x) sort(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define inf 0x3f3f3f3f
 
-vector<tuple<int, int, int>> graph[2010];
-int sp[2010][2010];
+const int MM = 2e3+5;
+int K, N, M, A, B, sp[MM][205], vis[MM];
+vector<pair<int, pii>> adj[MM];
 
 int main() {
-    int k, n, m;
-    cin >> k >> n >> m;
-    //initialize graph
-    for (int i = 0; i < m; i++) {
-        int a, b, time, hullCost;
-        cin >> a >> b >> time >> hullCost;
-        graph[a].emplace_back(make_tuple(time, b, hullCost));
-        graph[b].emplace_back(make_tuple(time, a, hullCost));
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> K >> N >> M;
+    for (int i = 0, a, b, t, h; i < M; i++) {
+        cin >> a >> b >> t >> h;
+        adj[a].pb({t, {b, h}});
+        adj[b].pb({t, {a, h}});
     }
-
-    //initialize shortest path array
-    for (int i = 0; i < 2010; i++){
-        for (int j = 0; j < 2010; j++){
-            sp[i][j] = INT_MAX;
-        }
-    }
-    // get start and end
-    int start, end;
-    cin >> start >> end;
-
-    //initialize starting node
-    for (int i = 0; i < 2010; i++){
-        sp[start][i] = 0;
-    }
-
-    //initialize priority queue
-    priority_queue<tuple<int, int, int>> q;
-    q.push(make_tuple(0, start, 0));
-
-    //dijkstra's
-    while (!q.empty()) {
-        tuple<int, int, int> front = q.top();
-        // record node and pop it. Set it as visited
-        int time = -get<0>(front);
-        int node = get<1>(front);
-        int hullCost = get<2>(front);
-        q.pop();
-
-        // There is a shorter way to get to this node, continue
-        if (time > sp[node][hullCost]) continue;
-
-        // iterate through every node connected
-        for (int i = 0; i < graph[node].size(); i++) {
-            // node has not been visited
-            int connectedTime = get<0>(graph[node][i]);
-            int connectedNode = get<1>(graph[node][i]);
-            int connectedCost = get<2>(graph[node][i]);
-            // if there is a shorter path to the node, update the cost
-            if (sp[connectedNode][hullCost+connectedCost] >= time+connectedTime){
-                //Hull cost is more than current hull, continue
-                if (hullCost + connectedCost >= k) continue;
-                // Update time to get to node at current hull cost and push into queue
-                sp[connectedNode][hullCost+connectedCost] = sp[node][hullCost]+connectedTime;
-                q.push(make_tuple(-(connectedTime+time), connectedNode, connectedCost+hullCost));
+    cin >> A >> B;
+    memset(sp, 0x3f, sizeof(sp));
+    for (int i = 0; i <= 200; i++) sp[A][i] = 0;
+    priority_queue<pair<int, pii>> pq;
+    pq.push({0, {A, 0}});
+    while (!pq.empty()) {
+        int t = -pq.top().first;
+        int v = pq.top().second.first;
+        int h = pq.top().second.second;
+        pq.pop();
+        if (t > sp[v][h]) continue;
+        for (pair<int, pii> p : adj[v]) {
+            int ut = p.first, u = p.second.first, uh = p.second.second;
+            if (sp[u][h+uh] > t+ut) {
+                if (h + uh >= K) continue;
+                sp[u][uh+h] = sp[v][h] + ut;
+                pq.push({-t-ut, {u, h+uh}});
             }
         }
     }
-    int lowest = INT_MAX;
-    //Shortest distance while maintaining the required hull cost
-    for (int i = 0; i < k; i++){
-        if (sp[end][i] < lowest){
-            lowest = sp[end][i];
-        }
-    }
-    if (lowest == INT_MAX){
-        cout << -1;
-    }
-    else {
-        cout << lowest;
-    }
+    int ans = inf;
+    for (int i = 0; i <= 200; i++) ans = min(ans, sp[B][i]);
+    cout << (ans==inf?-1:ans) << '\n';
 }
