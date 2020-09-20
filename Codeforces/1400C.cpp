56a2a15ef11e4bf0d0dc24c6fc97a4631157dd67
Ryan Li <ryan.lichangrui@gmail.com>
2020-09-19 20:43:21 -0400

    1400 A+C

---
+++ Codeforces/1400C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define srt(x) sort(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define inf 0x3f3f3f3f
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        int x;
+        cin >> s >> x;
+        int n = s.size();
+        string w = string(n, '1');
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') {
+                if (i-x < 0 && i+x >= n) continue;
+                else if (i-x >= 0 && i+x < n) w[i-x] = '0', w[i+x] = '0';
+                else if (i-x < 0) w[i+x] = '0';
+                else if (i+x >= n) w[i-x] = '0';
+            }
+        }
+        bool valid = true;
+        for (int i = 0; i < n; i++) {
+            bool one = false;
+            one |= (i-x >= 0 && w[i-x] == '1');
+            one |= (i+x < n && w[i+x] == '1');
+            if (one && s[i] != '1') valid = false;
+            else if (!one && s[i] == '1') valid = false;
+        }
+        cout << (valid?w:"-1") << '\n';
+    }
+}
