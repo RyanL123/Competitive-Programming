cf27bbd73bb89bf2cc48225a7d82c97e8df301f8
Ryan <ryan.lichangrui@gmail.com>
2020-10-23 17:44:18 -0400

    redo 2000 s4

--- CCC C++/2000/S4.cpp
+++ CCC C++/2000/S4.cpp
@@ -1,45 +1,39 @@
-#include <iostream>
-#include <algorithm>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define srt(x) sort(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define inf 0x3f3f3f3f
 
 int dp[5300];
+
 int main() {
-    int distance, c, clubs[110];
-    cin >> distance >> c;
-    //Get clubs input
-    for (int i = 0; i < c; i++){
-        cin >> clubs[i];
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int d, n;
+    cin >> d >> n;
+    vi v;
+    for (int i = 0, x; i < n; i++) {
+        cin >> x;
+        v.pb(x);
     }
-    for (int i = 0; i < 5300; i++){
-        dp[i] = INT_MAX;
-    }
-
-    //Total distance is 0
+    srt(v);
+    memset(dp, 0x3f, sizeof(dp));
     dp[0] = 0;
-    sort(clubs, clubs+c);
-
-    //Bottom up DP
-    for (int i = 1; i <= distance; i++){
-        for (int j = 1; j <= c; j++){
-            //Club's distance is less than total distance
-            if (clubs[j-1] <= i){
-                //Amount of swings using the club
-                int subDistance = dp[i-clubs[j-1]]+1;
-                //Prevent integer overflow
-                if (subDistance != INT_MAX){
-                    //Only use the club if the # of strokes is less than what is
-                    //currently possible
-                    dp[i] = min(subDistance, dp[i]);
-                }
-            }
+    for (int i = 0; i < 5300; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i-v[j] < 0) continue;
+            dp[i] = min(dp[i], dp[i-v[j]]+1);
         }
     }
-    if (dp[distance] == INT_MAX){
-        cout << "Roberta acknowledges defeat.";
-    }
-    else {
-        printf("Roberta wins in %i strokes.", dp[distance]);
-    }
+    if (dp[d] >= inf) cout << "Roberta acknowledges defeat.\n";
+    else cout << "Roberta wins in " << dp[d] << " strokes.\n";
 }
