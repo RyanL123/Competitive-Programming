a4f80be0f944bfda92546c93a8805a80c6533792
Ryan <ryan.lichangrui@gmail.com>
2020-11-12 18:08:28 -0500

    redo 2010 j4

--- CCC C++/2010/J4.cpp
+++ CCC C++/2010/J4.cpp
@@ -1,68 +1,37 @@
-#include <iostream>
-#include <cstdio>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> vii;
+typedef vector<int> vi;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define srt(x) sort(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+#define inf 0x3f3f3f3f
 
-int main(){
-
-    int n, x;
-
-    while (true){
-        again :
-        scanf("%i", &n);
-
-        if (n == 0){
-            break;
-        }
-
-        //gets temperatures
-        int temp[n];
-        for (int i = 0; i < n; i++){
-            scanf("%i", &x);
-            temp[i] = x;
-        }
-
-        //sequence has length of 1
-        if (n == 1){
-            printf("0\n");
-            goto again;
-        }
-        else if (n == 2){
-            printf("1\n");
-            goto again;
-        }
-
-        //stores difference between temps
-        int difference [50] {0};
-
-        for (int i = 0; i < n; i++){
-            if (i != n-1){
-                difference[i] = temp[i+1]-temp[i];
-            }
-        }
-
-        //finds the shortest difference
-        int length = 1;
-        while (length != n){
-            bool repeats = true;
-            for (int i = 0; i < n-length-1; i++){
-                if (difference[i] != difference[i+length]){
-                    repeats = false;
-                    break;
+int a[25], d[25];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+    while (cin >> N) {
+        if (N == 0) break;
+        for (int i = 0; i < N; i++) cin >> a[i];
+        for (int i = 1; i < N; i++) d[i] = a[i]-a[i-1];
+        int ans = N-1;
+        for (int i = 1; i < N; i++) {
+            bool flag = true;
+            for (int j = 1; j < N; j++) {
+                if (i+j < N) {
+                    flag &= (d[j] == d[j+i]);
                 }
             }
-            if (repeats){
-                break;
-            }
-            length++;
+            if (flag) ans = min(ans, i);
         }
-
-        //if there are no repeating patterns the length is the entire pattern
-        if (length == n){
-           printf("%i\n", n-1);
-        }
-        else {
-            printf("%i\n", length);
-        }
+        cout << ans << '\n';
     }
 }
