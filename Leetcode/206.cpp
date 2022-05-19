c358cd819049fcd7b860861df3006ce011c6fb11
RyanL123 <ryan.lichangrui@gmail.com>
2022-05-19 13:26:46 -0400

    day10: 21 + 206

---
+++ Leetcode/206.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* ret = reverseList(head->next), *tmp = ret;
+        while (tmp->next) {
+            tmp = tmp->next;
+        }
+        tmp->next = head;
+        tmp->next->next = nullptr;
+        return ret;
+    }
+};
